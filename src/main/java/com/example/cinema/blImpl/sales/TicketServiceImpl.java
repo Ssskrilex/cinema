53e222d8a348abf3c9a5aed28e681c70537e7239
任午阳 <18800179119@139.com>
2019-06-04 20:15:54 +0800

    移植

---
+++ src/main/java/com/example/cinema/blImpl/sales/TicketServiceImpl.java
@@ -0,0 +1,105 @@
+package com.example.cinema.blImpl.sales;
+
+import com.example.cinema.bl.sales.TicketService;
+import com.example.cinema.blImpl.management.hall.HallServiceForBl;
+import com.example.cinema.blImpl.management.schedule.ScheduleServiceForBl;
+import com.example.cinema.data.sales.TicketMapper;
+import com.example.cinema.po.Hall;
+import com.example.cinema.po.ScheduleItem;
+import com.example.cinema.po.Ticket;
+import com.example.cinema.vo.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Created by liying on 2019/4/16.
+ */
+@Service
+public class TicketServiceImpl implements TicketService {
+
+    @Autowired
+    TicketMapper ticketMapper;
+    @Autowired
+    ScheduleServiceForBl scheduleService;
+    @Autowired
+    HallServiceForBl hallService;
+
+    @Override
+    @Transactional
+    public ResponseVO addTicket(TicketForm ticketForm) {
+        try{
+            List<Ticket> tickets = null;
+            List<TicketVO> TicketVOS = null;
+            for(int i = 0; i < ticketForm.getSeats().size(); i++) {
+                Ticket ticket = new Ticket();
+                ticket.setId(ticketForm.getUserId());
+                ticket.setScheduleId(ticketForm.getScheduleId());
+                ticket.setColumnIndex(ticketForm.getSeats().get(i).getColumnIndex());
+                ticket.setRowIndex(ticketForm.getSeats().get(i).getRowIndex());
+                tickets.add(ticket);
+                TicketVOS.add(ticket.getVO());
+            }
+            ticketMapper.insertTickets(tickets);
+           return ResponseVO.buildSuccess(TicketVOS);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseVO.buildFailure("失败");
+        }
+    }
+
+    @Override
+    @Transactional
+    public ResponseVO completeTicket(List<Integer> id, int couponId) {
+        return null;
+    }
+
+    @Override
+    public ResponseVO getBySchedule(int scheduleId) {
+        try {
+            List<Ticket> tickets = ticketMapper.selectTicketsBySchedule(scheduleId);
+            ScheduleItem schedule=scheduleService.getScheduleItemById(scheduleId);
+            Hall hall=hallService.getHallById(schedule.getHallId());
+            int[][] seats=new int[hall.getRow()][hall.getColumn()];
+            tickets.stream().forEach(ticket -> {
+                seats[ticket.getRowIndex()][ticket.getColumnIndex()]=1;
+            });
+            ScheduleWithSeatVO scheduleWithSeatVO=new ScheduleWithSeatVO();
+            scheduleWithSeatVO.setScheduleItem(schedule);
+            scheduleWithSeatVO.setSeats(seats);
+            return ResponseVO.buildSuccess(scheduleWithSeatVO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseVO.buildFailure("失败");
+        }
+    }
+
+    @Override
+    public ResponseVO getTicketByUser(int userId) {
+        return null;
+
+    }
+
+    @Override
+    @Transactional
+    public ResponseVO completeByVIPCard(List<Integer> id, int couponId) {
+        return null;
+    }
+
+    @Override
+    public ResponseVO cancelTicket(List<Integer> id) {
+        try {
+            ticketMapper.updateTicketState(id, 2);
+            return ResponseVO.buildSuccess("订单已失效");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseVO.buildFailure("失败");
+        }
+    }
+
+
+
+}
